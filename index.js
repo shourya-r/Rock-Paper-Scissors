40a049de6deee3350fd2dd9bb3738c58c56dcf28
shourya-r <rajputshourya7@gmail.com>
2024-01-03 13:34:06 +0530

    Added the result 2D array
    It will get the result based on what the player and computer choices are

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function getComputerChoice(list){
+// Function to get random element from a list
+function getRandom(list){
     return list[Math.floor(Math.random()*list.length)];
 }
 
+// List that represents the choices available
 let choices = ["rock", "paper", "scissors"];
+
+// Function to get the computer's choice using getRandom
+function getComputerChoice(){
+    return getRandom(choices);
+}
+
+// 2D Result Array
+let resultArr = [
+//         R P S
+    /*R*/ [1,0,2]
+    /*P*/ [2,1,0]
+    /*S*/ [0,2,1]
+]
 
